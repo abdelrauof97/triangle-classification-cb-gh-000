7f387df360623ddc63a0be02517ad6e7aa40bad1
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-22 23:02:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     
     if a <= 0 || b <= 0 || c <= 0 
       raise TriangleError
-    elsif self.false_triangle
+    elsif a + b <= c || a + c <= b || b + c <= a
       raise TriangleError
     elsif a == b && b == c 
       :equilateral
